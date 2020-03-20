ac0e999982185dbd159acfd0c338ae8e9c0d8b98
Edgar Eduardo Espindola <edgareduardoespindola@gmail.com>
2020-03-20 07:36:00 -0600

    Fixing sintax error

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -8,6 +8,7 @@ class ImageUploader < CarrierWave::Uploader::Base
     storage :fog
   else
     storage :file
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
