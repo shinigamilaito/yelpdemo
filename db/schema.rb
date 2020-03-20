708413b51dd5d00c6c6e4e90d8cb323ce22d8db4
Edgar Eduardo Espindola <edgareduardoespindola@gmail.com>
2020-03-20 07:27:10 -0600

    Uploaded images with carrierwave, S3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200319040816) do
+ActiveRecord::Schema.define(version: 20200320043925) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20200319040816) do
     t.string "website"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
 end
