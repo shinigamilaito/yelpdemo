768a8cecb552cf357d3f2de8735a50c3334f1354
Edgar Eduardo Espindola <edgareduardoespindola@gmail.com>
2020-03-21 23:38:19 -0600

    Link Reviews to Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200322050320) do
+ActiveRecord::Schema.define(version: 20200322052026) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 20200322050320) do
     t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
