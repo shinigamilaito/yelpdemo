2a60b5b0385a5d94a007dcfbee98f82e60f8d64f
Edgar Eduardo Espindola <edgareduardoespindola@gmail.com>
2020-03-22 13:33:14 -0600

    Update New Review form with clickable star ratings

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,11 @@ class Restaurant < ApplicationRecord
 
   has_many :reviews
 
+  def avg_rating
+    if reviews.present?
+      reviews.average(:rating).round(2, :down)
+    else
+      0
+    end
+  end
 end
